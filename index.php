dee2db292703ba16ebf73830fde2d8af954400b5
tung wei-i <ericliliwayne@gmail.com>
2022-05-10 01:29:31 +0800

    修正日曆排版+CSS美化及增加影片背景

--- index.php
+++ index.php
@@ -6,40 +6,96 @@
       box-sizing: border-box;
       margin: 0;
     }
+    
     .table{
       display: flex;
       flex-wrap: wrap;
-      width: 750px;
-      height: 750px;
-      border: 1px solid cadetblue;
+      width: 820px;
+      height: 820px;
+      /* border: 1px solid cadetblue; */
       justify-content:space-between;
+      align-content:center;
       margin: auto;
-      background-color: lightgreen;
+      box-shadow: 10px 10px 10px black;
+      backdrop-filter: blur(5px);
+      padding: 50PX;
+      /* background-color: lightgreen; */
     }
-    .table>div{
+    .table>.weekend,.workday{
       min-width: 100px;
       min-height: 100px;
       max-width: 100%;
       max-height: 100%;
-      border: 1px solid black;
-      margin: 1px auto;
-      
+      /* border: 1px solid black; */
+      margin: 2px auto;
+      opacity: 0.7;
+      box-shadow: 5px 5px 10px black;
+    }
+    .weekend:hover,.workday:hover {/*將區塊做水平翻轉*/
+    animation: 0.4s linear 0s alternate none 1 featuresicon;
+    -webkit-animation: 0.4s linear 0s alternate none 1 featuresicon;
+    color: #4183C4;
+    text-shadow: 3px 3px 5px black;
+    }
+    @keyframes featuresicon { 
+      50% {
+        transform: scaleX(0);
+    }
+      100% {
+        transform: scaleX(1);
+    }
+    }
+    @-webkit-keyframes featuresicon { 
+      50% {
+        -webkit-transform: scaleX(0);
+    }
+      100% {
+        -webkit-transform: scaleX(1);
+    }
+    }
+    .header0{
+      font-family: '標楷體';
+      font-size: 30px;
+      text-align: center;
+      font-weight: bolder;
+      line-height: 170%;
+      background-color: black;
+      color: white;
+      width: 100%;
+      height: calc(100% / 14);
+      border-bottom: 1px solid white;
+      margin: 0 auto;
+      box-shadow: 5px 5px 10px black;
     }
     .header{
       font-family: '標楷體';
       font-size: 50px;
+      font-weight: bolder;
       text-align: center;
-      line-height: 200%;
+      line-height: 100%;
       background-color: black;
       color: white;
+      width: calc(100% / 7);
+      height: calc(100% / 14);
+      border-bottom: 1px solid white;
+      border-left: 1px solid white;
+      border-right: 1px solid white;
+      margin: 0 auto;
+      box-shadow: 5px 5px 10px black;
     }
     .header2{
       font-family: '標楷體';
       font-size: 50px;
       text-align: center;
-      line-height: 200%;
+      font-weight: bolder;
+      line-height: 100%;
       color: red;
       background-color: black;
+      width: calc(100% / 7);
+      height: calc(100% / 14);
+      border-bottom: 1px solid white;
+      margin: 0 auto;
+      box-shadow: 5px 5px 10px black;
     }
 
     .workday{
@@ -47,25 +103,44 @@
       font-size: 80px;
       text-align: center;
       background-color: lightgray;
+      border: 1px solid lightgray;
     }
     .weekend{
       color: red;
       font-size: 80px;
       text-align: center;
       background-color: lightpink;
+      border: 1px solid lightpink;
     }
     .today{
-      color: blue;
+      background: linear-gradient(to top, #FFBFBF, #C2C2FF, #00FF00);
+      background: -webkit-linear-gradient(to top, #FFBFBF, #C2C2FF, #00FF00);
+      background-clip: text;
+      -webkit-background-clip: text;
+      /* background-color: lightgray; */
+      color: transparent;
       font-size: 80px;
       font-weight: bolder;
       text-align: center;
-      
+      border: none;
+    }
+    #myVideo{
+      width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
     }
   </style>
 <body>
 <h1>萬年曆</h1>
 <?php
 /*請在這裹撰寫你的萬年曆程式碼*/  
+  date_default_timezone_set("Asia/Taipei");//調整時區
   $month=date('n');
   $year=date('Y');
   $firstday=date('Y-').$month.('-1');
@@ -76,7 +151,7 @@
   $lastweekday=date('w',strtotime($lastday));
   $day2=date('j');
   $datedays=[];
-date_default_timezone_set("Asia/Taipei");//調整時區
+
 $week=['星期日','星期一','星期二','星期三','星期四','星期五','星期六'];
 
   
@@ -95,16 +170,23 @@ $week=['星期日','星期一','星期二','星期三','星期四','星期五','
   // print_r($datedays);
   // echo "</pre>";
   echo "<p style='font-size:45px;font-family:標楷體;color:red'>今天是 $today ".$week[date('w')]."</p>";//輸出今天日期星期
+  
 ?>
-<section class="table">
+<video autoplay muted loop id="myVideo">
+  <source src="./resource/video.mp4" type="video/mp4">
+</video>
 
-  <div class="header2">SUN</div>
-  <div class="header">MON</div>
-  <div class="header">TUE</div>
-  <div class="header">WED</div>
-  <div class="header">THU</div>
-  <div class="header">FRI</div>
-  <div class="header2">SAT</div>
+<section class="table">
+  <?php
+  echo "<div class='header0'>$year 年 ".$month." 月份日曆</div>";
+  ?>
+  <div class="header2">日</div>
+  <div class="header">一</div>
+  <div class="header">二</div>
+  <div class="header">三</div>
+  <div class="header">四</div>
+  <div class="header">五</div>
+  <div class="header2">六</div>
   
 <?php
   foreach($datedays as $k => $day){
